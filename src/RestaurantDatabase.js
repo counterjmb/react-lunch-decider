e1695183784052835a5f099724776f1466a75c14
jbeagle <James.Beagle@nelnet.net>
2021-06-29 15:16:34 -0500

    Adding more restaurants

--- src/RestaurantDatabase.js
+++ src/RestaurantDatabase.js
@@ -11,6 +11,24 @@ const restaurants=[
 		}, {
     "restaurant": "Saigon",
     "type": "Vietnamese"
+		}, {
+    "restaurant": "Applebees",
+    "type": "Trash"
+		}, {
+    "restaurant": "Cheddars",
+    "type": "Better Applebees"
+		}, {
+    "restaurant": "McDonalds",
+    "type": "Delicious Trash"
+		}, {
+    "restaurant": "Jimmy Johns",
+    "type": "Sammy"
+		}, {
+    "restaurant": "Bahn Mi Shop",
+    "type": "Vietnamese"
+		}, {
+    "restaurant": "Golden Coral",
+    "type": "Early Bird Special"
     }, {
 		"restaurant": "Arby's",
 		"type": "Trash"
